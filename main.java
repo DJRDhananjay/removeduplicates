a283a7438100c705f827775178179475c6ca5f4d
DJRDhananjay <128344998+DJRDhananjay@users.noreply.github.com>
2026-04-13 21:55:22 +0530

    Create main.java
    Remove Duplicates from Arraylist

---
+++ main.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		ArrayList<Integer>list=new ArrayList(Arrays.asList(1,2,3,4,4,5,5));
+		HashSet<Integer>set=new HashSet(list);
+	    System.out.print(set);
+	    
+	}}
